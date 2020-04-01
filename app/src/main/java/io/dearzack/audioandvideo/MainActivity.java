47157e9f5e7d5242ca2f4cd5b5baa1b5575e22dd
Zack <zack4work@gmail.com>
2020-04-01 23:01:55 +0800

    init

---
+++ app/src/main/java/io/dearzack/audioandvideo/MainActivity.java
@@ -0,0 +1,20 @@
+package io.dearzack.audioandvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.rv_main);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+    }
+}
